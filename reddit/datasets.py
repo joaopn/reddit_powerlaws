24feb950b46bf70d58782d3ead72f4bedb2d2158
Joao Neto <joaoxp@gmail.com>
2021-03-05 22:25:32 +0100

    8X Speedup in dataset parsing
    Massive speed-up in parsing using caching and groupby

--- reddit/datasets.py
+++ reddit/datasets.py
@@ -2,7 +2,7 @@
 # @Author: joaopn
 # @Date:   2021-03-02 00:44:06
 # @Last Modified by:   joaopn
-# @Last Modified time: 2021-03-03 14:39:49
+# @Last Modified time: 2021-03-05 22:24:02
 
 '''
 Module for handling the reddit data dump available at https://files.pushshift.io/reddit/
@@ -33,19 +33,7 @@ def count_subreddits(file, savefile=None, chunksize=1000):
 
 	return subreddits
 
-def split_subreddits(file, subreddit_list, save_path, fields, chunksize=1000):
-	"""Processes a submission dump, splitting the data into a CSV file for each subreddit.
-	
-	Args:
-		file (TYPE): Description
-		subreddit_list (TYPE): Description
-		save_path (TYPE): Description
-		chunksize (int, optional): Description
-	"""
-
-	pass
-
-def subreddits_hdf5(file, savefile, chunksize=100000, drop_stickied = True):
+def subreddits_hdf5(file, savefile, chunksize=100000, mem_limit = 1000, drop_stickied = True):
 	"""Parses a submission dump dataset into an HDF5 file, where each group is a subreddit.
 	
 	Args:
@@ -58,10 +46,14 @@ def subreddits_hdf5(file, savefile, chunksize=100000, drop_stickied = True):
 
 	#Fixed parameters
 	fields = ['subreddit', 'author', 'domain', 'created_utc','num_comments', 'score', 'id']
-
 	field_dtypes = {'subreddit':str, 'author':str, 'domain':str, 'created_utc':int,'num_comments':int, 'score':int, 'id':str}
 
+	#File object
 	save_obj = pd.HDFStore(savefile,mode='a', complevel=9)
+
+	#Stores caching data
+	results_list = []
+	mem_used = 0
 
 	for df in pd.read_json(file, lines = True, chunksize=chunksize, dtype=field_dtypes):
 
@@ -72,6 +64,32 @@ def subreddits_hdf5(file, savefile, chunksize=100000, drop_stickied = True):
 		#Drops other columns
 		df = df[fields]
 
-		#Adds data to df
+		#Adds caching data
+		mem_used += df.memory_usage().sum()/1024/1024
+		results_list.append(df)
+
+		#Dumps data if mem_used > mem_limit
+		if mem_used > mem_limit:
+
+			df = pd.concat(results_list)
+			grouped_df = df.groupby('subreddit')
+
+			for subreddit in df['subreddit'].unique():
+				save_obj.put('/' + subreddit, grouped_df.get_group(subreddit), append=True, format='table', min_itemsize=255)
+
+			results_list = []
+			mem_used = 0
+			last_saved = True #ugly hack because python lacks an iterator check
+		else:
+			last_saved = False
+
+	#Saves last piece of data
+	if last_saved is False:
+		df = pd.concat(results_list)
+		grouped_df = df.groupby('subreddit')
 		for subreddit in df['subreddit'].unique():
-			save_obj.put('/' + subreddit, df[df['subreddit'] == subreddit], append=True, format='table', min_itemsize=255)
+			save_obj.put('/' + subreddit, grouped_df.get_group(subreddit), append=True, format='table', min_itemsize=255)
+
+	#Closes object
+	save_obj.close()
+
