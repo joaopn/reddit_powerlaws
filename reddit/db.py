c0183817e34860f9ce750204ebbfdeefeb1a2c9e
Joao Neto <joaoxp@gmail.com>
2021-04-10 18:57:16 +0200

    Updates db functions
    Adds get_subreddit_count_single, adds docstrings, tidy it up

--- reddit/db.py
+++ reddit/db.py
@@ -4,18 +4,19 @@ Module with functions to interface with and maintain the dataset MongoDB databas
 
 from pymongo import MongoClient
 
-
 def get_num_comments(subreddit, db='pushshift', collection='submissions', batch_size=1000000, db_address=
 '127.0.0.1:27017'):
 	"""
 	Loads a list of number of comments in a submission.
 	Args:
 		subreddit (str): Subreddit to get data from
+		db (str): DB name
+		collection (str): collection name
 		batch_size: maximum number of documents to return per query. Limited to 16MB of data.
+		db_address (str): IP address of the DB
 
 	Returns:
 		list of number of comments (num_comments)
-
 	"""
 
 	client = MongoClient('mongodb://' + db_address)
@@ -26,21 +27,69 @@ def get_num_comments(subreddit, db='pushshift', collection='submissions', batch_
 
 
 def update_subreddit_count(db='pushshift', collection='submissions', db_address='127.0.0.1:27017'):
+	"""
+	Updates the db collection [collection]_statistics, which stores # of submissions and comments in the subreddit.
+	Args:
+		db (str): DB name
+		collection (str): collection name
+		db_address (str): IP address of the DB
 
+	"""
 	client = MongoClient('mongodb://' + db_address)
 
-	client[db][collection].aggregate([
+	pipeline = [
+    {'$match': {'subreddit': {'$ne': None}}},
+		{'$project': {'subreddit': 1, '_id': 0, 'num_comments': 1}},
 		{'$group': {'_id': '$subreddit',
 		            'submissions': {'$sum': 1},
 		            'comments': {'$sum': '$num_comments'}}},
 		{'$sort': {'submissions': -1}},
-		{'$out': collection + '_statistics'}
-	], **{'allowDiskUse':True})
+		{'$out': 'subreddit_statistics'}]
+
+	client[db][collection].aggregate(pipeline, **{'allowDiskUse': True})
+
 
 def get_subreddit_count(db='pushshift', collection='submissions', batch_size=1000000, db_address='127.0.0.1:27017'):
+	"""
+	Gets the DB statistics from [collection]_statistics, which stores # of submissions and comments in the subreddit.
+	Args:
+		db (str): DB name
+		collection (str): collection name
+		db_address (str): IP address of the DB
+
+	Returns:
+		list of dict results containings  {'_id': [subreddit name], 'submissions': int, 'comments': int}
+	"""
 
 	client = MongoClient('mongodb://' + db_address)
 
 	result = client[db][collection + '_statistics'].find(batch_size=batch_size)
 
+	return list(result)
+
+
+def get_subreddit_count_single(subreddit, db='pushshift', collection='submissions', batch_size=1000000,
+                               db_address='127.0.0.1:27017'):
+
+	"""
+	Returns the subreddit statistics from a single subreddit, reading the DB directly.
+	Args:
+		db (str): DB name
+		collection (str): collection name
+		db_address (str): IP address of the DB
+
+	Returns:
+		list of dict results containings  {'_id': [subreddit name], 'submissions': int, 'comments': int}
+	"""
+
+	client = MongoClient('mongodb://' + db_address)
+	pipeline = [
+		{'$match': {'subreddit': subreddit}},
+		#{'$project': {'subreddit': 1, '_id': 0, 'num_comments': 1}},
+		{'$group': {'_id': '$subreddit',
+		            'num_comments': {'$sum': '$num_comments'},
+		            'submissions': {'$sum': 1}
+		            }}
+	]
+	result = client[db][collection].aggregate(pipeline,**{'allowDiskUse': True, 'batchSize':batch_size})
 	return list(result)
