adb5961386ea6321db34c19bebf3b17d72ad2fca
Joao Neto <joaoxp@gmail.com>
2021-04-17 16:39:32 +0200

    Updates db functions, adds db.add_data

--- reddit/db.py
+++ reddit/db.py
@@ -5,6 +5,42 @@ Module with functions to interface with and maintain the dataset MongoDB databas
 from pymongo import MongoClient
 import numpy as np
 
+def add_data(db_from, db_to, collection_from, collection_to, fields_selection, db_address=
+'127.0.0.1:27017'):
+
+	"""
+	Copies selected fields from a database collection to another.
+	Args:
+		db_from (str): origin database
+		db_to (str): target database
+		collection_from (str): origin collection
+		collection_to (str): target collection
+		fields_selection (str): Set of fields to copy. Valid options: 'comments','submissions'
+
+	Returns:
+		None
+	"""
+
+	global fields
+	if fields_selection == 'comments':
+		fields = ['author', 'created_utc', 'id', 'subreddit', 'score', 'link_id', 'parent_id']
+	elif fields_selection == 'submissions':
+		fields = ['author', 'created_utc', 'id', 'subreddit', 'score', 'domain', 'num_comments']
+	else:
+		raise ValueError('field_selection must be "comments" or "submissions"')
+
+	client = MongoClient('mongodb://' + db_address)
+
+	project = {field:1 for field in fields}
+	project['_id'] = 0
+
+	pipeline = [
+    {'$project': project},
+	{'$merge':{'into':{'db': db_to, 'coll': collection_to}}}
+	]
+
+	client[db_from][collection_from].aggregate(pipeline, **{'allowDiskUse': True})
+
 def add_collection(collection, db, db_address=
 '127.0.0.1:27017'):
 	"""
@@ -20,7 +56,7 @@ def add_collection(collection, db, db_address=
 	client = MongoClient('mongodb://' + db_address)
 	client[db].create_collection(collection, storageEngine={'wiredTiger': {'configString': "block_compressor=zstd"}})
 
-def get_num_comments(subreddit, db='pushshift', collection='submissions', batch_size=1000000, db_address=
+def get_num_comments(subreddit, db='reddit', collection='submissions', batch_size=1000000, db_address=
 '127.0.0.1:27017'):
 	"""
 	Loads a list of number of comments in a submission.
@@ -42,7 +78,7 @@ def get_num_comments(subreddit, db='pushshift', collection='submissions', batch_
 	return np.array([x['num_comments'] for x in result_list])
 
 
-def update_subreddit_count(db='pushshift', collection='submissions', db_address='127.0.0.1:27017'):
+def add_subreddit_statistics(db='reddit', collection='submissions', db_address='127.0.0.1:27017'):
 	"""
 	Updates the db collection [collection]_statistics, which stores # of submissions and comments in the subreddit.
 	Args:
@@ -65,7 +101,7 @@ def update_subreddit_count(db='pushshift', collection='submissions', db_address=
 	client[db][collection].aggregate(pipeline, **{'allowDiskUse': True})
 
 
-def get_subreddit_count(db='pushshift', collection='submissions', batch_size=1000000, db_address='127.0.0.1:27017'):
+def get_subreddit_statisics(db='reddit', collection='submissions', batch_size=1000000, db_address='127.0.0.1:27017'):
 	"""
 	Gets the DB statistics from [collection]_statistics, which stores # of submissions and comments in the subreddit.
 	Args:
@@ -84,7 +120,7 @@ def get_subreddit_count(db='pushshift', collection='submissions', batch_size=100
 	return list(result)
 
 
-def get_subreddit_count_single(subreddit, db='pushshift', collection='submissions', batch_size=1000000,
+def get_subreddit_count_single(subreddit, db='reddit', collection='submissions', batch_size=1000000,
                                db_address='127.0.0.1:27017'):
 
 	"""
